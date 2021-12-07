7a0240844ab5948fc56029749c47c9220e5739b4
Bruno Martins <bruno.martins217@gmail.com>
2021-12-07 17:05:51 -0300

    criando o componente modal de conversao de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { ConversorComponent } from './components'
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
+import { ModalCotacaoComponent } from './utils';
 
 @NgModule({
   declarations: [
     ConversorComponent,
-    NumeroDirective
+    NumeroDirective,
+    ModalCotacaoComponent
   ],
   imports: [
     CommonModule,
