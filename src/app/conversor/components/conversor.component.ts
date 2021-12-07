a3905aa898a65b1bb0c13b223f8dbece15f479cf
Bruno Martins <bruno.martins217@gmail.com>
2021-12-06 23:42:31 -0300

    implementando o formulario de conversao de moedas

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
 
 @Component({
   selector: 'app-conversor',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conversor.component.css']
 })
 export class ConversorComponent implements OnInit {
+  @ViewChild("conversaoForm", { static: true }) conversaoForm: NgForm;
 
-  constructor() { }
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
+
+  constructor(
+    private moedaService : MoedaService,
+    private conversorService: ConversorService
+  ) { }
 
   ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
   }
 
+  init(): void {
+    this.conversao = new Conversao('EUR', 'BRL', null);
+    this.possuiErro = false;
+  }
+
+  converter(): void {
+    if (this.conversaoForm.form.valid) {
+      alert('Convertendo: ' + JSON.stringify(this.conversao));
+    }
+  }
 }
