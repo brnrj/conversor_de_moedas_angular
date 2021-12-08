29655d8fee5448ef14f2374ff69e12f2f84f71f3
Bruno Martins <bruno.martins217@gmail.com>
2021-12-08 00:33:40 -0300

    ajustando testes unitarios

--- src/app/conversor/pipes/data-br.pipe.spec.ts
+++ src/app/conversor/pipes/data-br.pipe.spec.ts
@@ -7,6 +7,7 @@ describe('DataBrPipe', () => {
   });
   it('deve formatar a data 12-08-2021 para 08/12/2021', () => {
     const pipe = new DataBrPipe();
-    expect(pipe.transform('12-08-2021')).toEqual('08/12/2021');
+    expect(pipe.transform('2021-12-08')).toEqual('08/12/2021');
+    console.log(pipe.transform('12-08-2021'));
   })
 });
